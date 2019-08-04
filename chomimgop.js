5450ab418e598c325a4dce99550aaf7c9b320b53
Chomtana <chomtana001@gmail.com>
2019-08-04 15:11:06 +0700

    make it sequen and fix cb

--- chomimgop.js
+++ chomimgop.js
@@ -20,7 +20,7 @@ function fileext(file) {
 }
 
 exports.default = function chomimgop() {
-  return through.obj(function chomimgop(file, enc, cb) {
+  return through.obj(async function chomimgop(file, enc, cb) {
     var filepath = file.path;
 
     //this is the result file don't optimize it again
@@ -30,69 +30,87 @@ exports.default = function chomimgop() {
     }
 
     function w(width) {
-      var ext = fileext(filepath);
-      var suffix = width ? "-" + width + "w" : "";
+      return new Promise((resolve, reject) => {
+        var ext = fileext(filepath);
+        var suffix = width ? "-" + width + "w" : "";
 
-      if (!width) width = 1920;
+        if (!width) width = 1920;
 
-      gm(filepath)
-        .quality(60)
-        .resize(width, null, ">")
-        .write(changeext(namesuffix(filepath, suffix), "webp"), err => {
-          if (err) {
-            console.log(namesuffix(filepath, suffix) + " Error!");
-            console.log(err);
-          } else {
-            console.log(namesuffix(filepath, suffix) + " Success!");
-          }
-        });
+        let webp_ok = false;
+        let base_ok = false;
 
-      if (ext.toLowerCase() === "jpg" || ext.toLowerCase() === "jpeg") {
         gm(filepath)
           .quality(60)
           .resize(width, null, ">")
-          .strip()
-          .samplingFactor(4, 2)
-          .interlace("JPEG")
-          .colorspace("RGB")
-          .write(namesuffix(filepath, suffix), err => {
+          .write(changeext(namesuffix(filepath, suffix), "webp"), err => {
             if (err) {
               console.log(namesuffix(filepath, suffix) + " Error!");
               console.log(err);
+              reject(err);
             } else {
               console.log(namesuffix(filepath, suffix) + " Success!");
+
+              webp_ok = true;
+              if (webp_ok && base_ok) resolve();
             }
           });
-      } else if (ext.toLowerCase() === "png") {
-        exec(
-          "pngquant " + filepath + " -f -o " + namesuffix(filepath, suffix),
-          (err, stdout, stderr) => {
-            if (err) {
-              console.log("PNGQuant error", err);
-              return;
-            }
 
-            // the *entire* stdout and stderr (buffered)
-            if (stdout.trim() || stderr.trim()) {
-              console.log(`PNGQuant stdout: ${stdout}`);
-              console.log(`PNGQuant stderr: ${stderr}`);
-            } else {
-              console.log(namesuffix(filepath, suffix) + " Success!");
+        if (ext.toLowerCase() === "jpg" || ext.toLowerCase() === "jpeg") {
+          gm(filepath)
+            .quality(60)
+            .resize(width, null, ">")
+            .strip()
+            .samplingFactor(4, 2)
+            .interlace("JPEG")
+            .colorspace("RGB")
+            .write(namesuffix(filepath, suffix), err => {
+              if (err) {
+                console.log(namesuffix(filepath, suffix) + " Error!");
+                console.log(err);
+                reject(err);
+              } else {
+                console.log(namesuffix(filepath, suffix) + " Success!");
+
+                base_ok = true;
+                if (webp_ok && base_ok) resolve();
+              }
+            });
+        } else if (ext.toLowerCase() === "png") {
+          exec(
+            "pngquant " + filepath + " -f -o " + namesuffix(filepath, suffix),
+            (err, stdout, stderr) => {
+              if (err) {
+                console.log("PNGQuant error", err);
+                reject(err);
+                return;
+              }
+
+              // the *entire* stdout and stderr (buffered)
+              if (stdout.trim() || stderr.trim()) {
+                console.log(`PNGQuant stdout: ${stdout}`);
+                console.log(`PNGQuant stderr: ${stderr}`);
+                if (stderr) reject(stderr); else reject(stdout);
+              } else {
+                console.log(namesuffix(filepath, suffix) + " Success!");
+                
+                base_ok = true;
+                if (webp_ok && base_ok) resolve();
+              }
             }
-          }
-        );
-      }
+          );
+        }
+      });
     }
 
-    w();
-    w(1920);
-    w(1600);
-    w(1280);
-    w(768);
-    w(480);
-    w(360);
-    w(240);
-    w(196);
+    await w();
+    await w(1920);
+    await w(1600);
+    await w(1280);
+    await w(768);
+    await w(480);
+    await w(360);
+    await w(240);
+    await w(196);
 
     cb();
   });
